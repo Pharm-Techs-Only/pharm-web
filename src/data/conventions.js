1765f61743700f667c8c7a995f8612be17953b45
Rich <rich@apollofractional.com>
2026-01-27 00:48:36 -0800

    conventions

--- src/data/conventions.js
+++ src/data/conventions.js
@@ -70,7 +70,7 @@ export const ConventionsData = [
     year: 2026,
     location: 'Kalahari Wisconsin Dells',
     date: '2026-08-27',
-    dateRange: 'August 27-29, 2026',
+    dateRange: 'August 27 - 29, 2026',
     website: 'https://www.pswi.org/Education/Conferences'
   },
   {
@@ -79,7 +79,7 @@ export const ConventionsData = [
     year: 2026,
     location: 'Kalahari Wisconsin Dells',
     date: '2026-03-27',
-    dateRange: 'March 27-30, 2026',
+    dateRange: 'March 27 - 30, 2026',
     website: 'https://cpe.cpha.com/'
   },
   {
@@ -88,7 +88,7 @@ export const ConventionsData = [
     year: 2026,
     location: 'Hilton, Harrisburg, PA',
     date: '2026-02-20',
-    dateRange: 'February 20-22, 2026',
+    dateRange: 'February 20 - 22, 2026',
     website: 'https://www.papharmacists.com/page/Annual2026'
   },
   {
@@ -97,7 +97,7 @@ export const ConventionsData = [
     year: 2026,
     location: 'Marriott Woodlands Waterway- Woodlands, TX',
     date: '2026-07-24',
-    dateRange: 'July 24-26, 2026',
+    dateRange: 'July 24 - 26, 2026',
     website: 'https://www.texaspharmacy.org/page/confexpo'
   },
   {
@@ -106,7 +106,7 @@ export const ConventionsData = [
     year: 2026,
     location: 'Montereey Plaza Hotel & Spa,  Monterey, CA',
     date: '2026-02-26',
-    dateRange: 'February 26-27, 2026',
+    dateRange: 'February 26 - 27, 2026',
     website: 'https://www.cshp.org/general/custom.asp?page=PCPSC-2026'
   },
   {
@@ -127,6 +127,60 @@ export const ConventionsData = [
     dateRange: 'May 28 - 31, 2026',
     website: 'https://www.wsparx.org/events/EventDetails.aspx?id=1990230&group='
   },
+  {
+    name: '2026 NPhA/SNPhA Annual Convention',
+    country: 'United States of America',
+    year: 2026,
+    location: 'Bethesda North Marriott Hotel & Conference Center, Rockville, MD',
+    date: '2026-07-31',
+    dateRange: 'July 31 - August 3, 2026',
+    website: 'https://nationalpharmaceuticalassociation.org/event-6488736'
+  },
+  {
+    name: 'PTEC 2026 Conference (The Pharmacy Technician Educators Council)',
+    country: 'United States of America',
+    year: 2026,
+    location: 'San Antonio, TX',
+    date: '2026-07-09',
+    dateRange: 'July 9 - 11, 2026',
+    website: 'https://ptec.org/page/2026Conference'
+  },
+  {
+    name: '11th Annual Connecticut Compounding Conference',
+    country: 'United States of America',
+    year: 2026,
+    location: 'Bristol DoubleTree Hotel, 42 Century Dr, Bristol, CT 06010',
+    date: '2026-04-08',
+    dateRange: 'April 8, 2026',
+    website: 'https://ctpharmacists.org/events-and-ce/events/#!event/2026/4/8/11th-annual-connecticut-compounding-conference'
+  },
+  {
+    name: 'NCAP Annual Convention - North Carolina Association of Pharmacists',
+    country: 'United States of America',
+    year: 2026,
+    location: 'Embassy Suites by Hilton – Raleigh Durham Research Triangle in Cary, NC',
+    date: '2026-05-31',
+    dateRange: 'May 31 - June 2, 2026',
+    website: 'https://ncap.memberclicks.net/index.php?option=com_jevents&task=icalrepeat.detail&evid=292&Itemid=115&year=2026&month=05&day=31&title=2026-ncap-annual-convention&uid=163e1f17a50a1257b6480c7248b253e3'
+  },
+  {
+    name: '60th Annual Convention & Tradeshow',
+    country: 'United States of America',
+    year: 2026,
+    location: 'Hotel Captain Cook- 939 W. 5th Avenue Anchorage, 99501',
+    date: '2026-02-20',
+    dateRange: 'February 20 - 22, 2026',
+    website: 'https://alaskapharmacy.org/event/60th-annual-convention-tradeshow/'
+  },
+  {
+    name: 'RX Fest',
+    country: 'United States of America',
+    year: 2026,
+    location: 'TBD: More info as it becomes available…',
+    date: '2026-12-22',
+    dateRange: 'TBD 2026',
+    website: 'https://www.pharmfest.com/rxfest'
+  },
 
   // United States - 2027
   {
@@ -135,17 +189,26 @@ export const ConventionsData = [
     year: 2027,
     location: 'Omni Amelia Island Resort and Plantation, Florida',
     date: '2027-06-17',
-    dateRange: 'June 17-20, 2027',
+    dateRange: 'June 17 - 20, 2027',
     website: 'https://www.gpha.org/pharmacy-conventions-future-dates/'
   },
   {
     name: 'PSW Annual Meeting - Pharmacy Society of Wisconsin',
     country: 'United States of America',
     year: 2027,
-    location: 'TBD',
-    date: '2027-01-01', // Placeholder since specific date is TBD
-    dateRange: 'TBD 2027',
+    location: 'Hyatt Regency KI Convention Center, Green Bay, WI',
+    date: '2027-08-26', // Placeholder since specific date is TBD
+    dateRange: 'August 26 - 28, 2027',
     website: 'https://www.pswi.org/Education/Conferences'
+  },
+  {
+    name: '146th Annual Convention & Trade Show',
+    country: 'United States of America',
+    year: 2027,
+    location: 'TBD',
+    date: '2027-12-22', // Placeholder since specific date is TBD
+    dateRange: 'TBD 2027',
+    website: 'https://www.aparx.org/page/43'
   },
 
   // United States - 2028
@@ -158,6 +221,15 @@ export const ConventionsData = [
     dateRange: 'August 24-26, 2028',
     website: 'https://www.pswi.org/Education/Conferences'
   },
+  {
+    name: '147th Annual Convention & Trade Show',
+    country: 'United States of America',
+    year: 2028,
+    location: 'The Lodge at Gulf State Park',
+    date: '2028-06-04',
+    dateRange: 'June 4 - June 7, 2028',
+    website: ' https://www.aparx.org/page/43'
+  },  
 
   // United States - 2029
   {
@@ -169,6 +241,15 @@ export const ConventionsData = [
     dateRange: 'TBD 2029',
     website: 'https://www.pswi.org/Education/Conferences'
   },
+  {
+    name: '148th Annual Convention & Trade Show',
+    country: 'United States of America',
+    year: 2029,
+    location: 'The Lodge at Gulf State Park',
+    date: '2029-06-10',
+    dateRange: 'June 10 - 13, 2029',
+    website: ' https://www.aparx.org/page/43'
+  },  
 
   // United States - 2030
   {
@@ -199,16 +280,16 @@ export const ConventionsData = [
     year: 2026,
     location: 'TCU Place, Saskatoon Saskatchewan',
     date: '2026-04-23',
-    dateRange: 'April 23-26. 2026',
+    dateRange: 'April 23 - 26. 2026',
     website: 'https://www.capho.org/capho-conference-2026'
   },
   {
     name: 'CAPT National Professional Development Conference',
     country: 'Canada',
     year: 2026,
-    location: 'TBD as more information becomes available...',
-    date: '2026-08-01',
-    dateRange: 'TBD 2026',
+    location: '970 Dixon Rd. Toronto Ontario',
+    date: '2026-05-29',
+    dateRange: 'May 29 - May 31, 2026',
     website: 'https://capt.ca/client/event/roster/eventRosterView.html?eventRosterId=32'
   },
   {
@@ -216,8 +297,8 @@ export const ConventionsData = [
     country: 'Canada',
     year: 2026,
     location: 'TBD as more information becomes available...',
-    date: '2026-08-01',
-    dateRange: 'TBD 2026',
+    date: '2026-05-01',
+    dateRange: 'May 1 - 2, 2026',
     website: 'https://www.bcpharmacy.ca/conference'
   },
   {
@@ -233,18 +314,18 @@ export const ConventionsData = [
     name: 'Pharmacy Technician Professional Development Conference',
     country: 'Canada',
     year: 2026,
-    location: 'TBD as more information becomes available...',
-    date: '2026-08-01',
-    dateRange: 'TBD 2026',
+    location: 'White Oaks Conference Resort in Niagara-on-the-Lake, Ontario',
+    date: '2026-05-08',
+    dateRange: 'May 8 - 9, 2026',
     website: 'https://www.linkedin.com/posts/ptpdc_ptpdc2026-pharmacytechnicians-healthcaresponsors-activity-7384371363886768129-N0pe'
   },
   {
     name: 'Pharmacy U',
     country: 'Canada',
     year: 2026,
-    location: 'TBD as more information becomes available...',
-    date: '2026-08-01',
-    dateRange: 'TBD 2026',
+    location: 'The International Centre- Toronto, ON',
+    date: '2026-04-11',
+    dateRange: 'April 11, 2026',
     website: 'https://events.pharmacyu.ca/toronto2025/home?code=VIP'
   },
   {
@@ -257,13 +338,22 @@ export const ConventionsData = [
     website: 'https://www.cshp.ca/Site/Professional-Development/Together_2023/Site/Content/Professional-Development/together-2023.aspx?hkey=e1a4a84a-6930-4bf4-8f4f-eef50018eb86'
   },
   {
-    name: '"FIP Montreal 2026 84th FIP World Congress of Pharmacy and Pharmaceutical Sciences"',
+    name: 'FIP Montreal 2026 84th FIP World Congress of Pharmacy and Pharmaceutical Sciences',
     country: 'Canada',
     year: 2026,
     location: 'Montreal',
     date: '2026-08-30',
     dateRange: 'August 30 - September 2, 2026',
     website: 'https://montreal2026.fip.org/'
+  },
+  {
+    name: '19th Annual Pharmacy Technician Conference',
+    country: 'Canada',
+    year: 2026,
+    location: 'Richmond BC, Canada- River Rock Hotel',
+    date: '2026-10-24',
+    dateRange: 'October 23 - 24, 2026',
+    website: 'https://ptsbc.ca/'
   },
 
   // Australia - 2025
@@ -273,7 +363,7 @@ export const ConventionsData = [
     year: 2025,
     location: 'Melbourne Conference and Exhibition Centre',
     date: '2025-11-27',
-    dateRange: 'November 27-29, 2025',
+    dateRange: 'November 27 - 29, 2025',
     website: 'https://adpha.au/cpd/mm'
   },
 
@@ -291,11 +381,30 @@ export const ConventionsData = [
     name: 'Pharmacy Connect 2026',
     country: 'Australia',
     year: 2026,
-    location: 'TBD as more information becomes available...',
+    location: 'Hyatt Regency Sydney',
     date: '2026-09-11',
     dateRange: 'September 9 - 11, 2026',
-    website: 'https://www.facebook.com/PharmacyConnectConference/'
+    website: 'https://pharmacyconnect.com.au'
   },
+  {
+    name: 'MM2026',
+    country: 'Australia',
+    year: 2026,
+    location: 'Brisbane Convention and Exhibition Centre',
+    date: '2026-10-24',
+    dateRange: 'October 22 - 24, 2026',
+    website: 'https://www.psa.org.au/networking-events/information-on-major-events-and-conferences/'
+  },
+  {
+    name: 'PSA26 National Confernce 2026 ',
+    country: 'Australia',
+    year: 2026,
+    location: 'ICC Sydney: International Conventrion Centere',
+    date: '2026-08-02',
+    dateRange: 'July 31 - August 2, 2026',
+    website: 'https://www.psa.org.au/networking-events/information-on-major-events-and-conferences/'
+  },
+  
   // Europe - 2026
   {
     name: 'PharmaPak Europe 2026- 25th Anniversary',
@@ -319,27 +428,27 @@ export const ConventionsData = [
     name: 'APTUK 2026 Annual Conference and Exhibition',
     country: 'Europe',
     year: 2026,
-    location: 'TBD as more information becomes available...',
-    date: '2026-08-01',
-    dateRange: 'TBD: 2026',
-    website: ''
+    location: 'Renissance Hotel, Heathrow, London',
+    date: '2026-09-18',
+    dateRange: 'September 18 - 19, 2026',
+    website: 'https://www.aptuk.org/events/aptuk-2026-conference-and-exhibition'
   },
   {
     name: '2026 EAFP Annual Conference - European Association of Faculties of Pharmacy',
     country: 'Europe',
     year: 2026,
-    location: 'TBD as more information becomes available...',
-    date: '2026-08-01',
-    dateRange: 'TBD: 2026',
+    location: 'Tartu, Estonia',
+    date: '2026-06-04',
+    dateRange: 'June 4 - 6, 2026',
     website: 'https://eafponline.eu/future-conferences/'
   },
   {
-    name: '10th International Conference on Future Pharma and Innovations',
+    name: '11th International Conference on Future Pharma and Innovations',
     country: 'Europe',
     year: 2026,
     location: 'Netherlands',
-    date: '2026-09-23',
-    dateRange: 'Septemmber 23 - 24, 2026',
+    date: '2026-03-24',
+    dateRange: 'March 24 - 25, 2026',
     website: 'https://futurepharmameetings.pharmaceuticalconferences.com/'
   },
 
@@ -411,7 +520,7 @@ export const ConventionsData = [
     year: 2026,
     location: 'Southern Japan Splendour Luxury Cruise - Tokyo, Japan',
     date: '2026-05-08',
-    dateRange: 'May 9 - 20, 2026',
+    dateRange: 'May 8 - 20, 2026',
     website: 'https://impactevents.eventsair.com/cmspreview/psaoffshore2026'
   },
 ]

00e4850461d2c9ece0dd0fe218a95e086678e6da
Rich <rich@apollofractional.com>
2026-02-06 01:11:14 -0800

    resources

---
+++ src/data/careers.js
@@ -0,0 +1,16 @@
+// Career Resources as a flat array for easier management
+export const ResourceData = [
+  {
+    title: 'LinkedIn Profile Makeover for Pharmacy Technicians',
+    description: '',
+    image: '/assets/images/profile-makeover.png',
+    pdf: '/assets/docs/linkedin-profile-makeover.pdf',
+  },
+  {
+    title: 'LinkedIn Profile Makeover for Pharmacy Technicians',
+    description: '',
+    image: '/assets/images/profile-makeover.png',
+    pdf: '/assets/docs/linkedin-profile-makeover.pdf',
+  },
+
+]
